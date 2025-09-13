4b7b773d5e97f3b6971f9ca7c808f834b2d8466b
Prithviraj-Koparde <prithvirajkoparde@gmail.com>
2025-09-13 13:16:54 +0530

    Array practice for prefix sum without inplace method

---
+++ src/PrefixSumArray.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+// given an array a return the prefix sum/ running sum in the array
+class lec19{
+    static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    static int[] PrefixSum(int [] arr){
+        int [] pref = new int[arr.length];
+        pref[0] = arr[0];
+
+        for (int i = 1; i<arr.length; i++){
+            pref[i] = pref[i-1] + arr[i];
+        }
+        return pref;
+    }
+}
+
+public class PrefixSumArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter size of array: ");
+        lec19 obj = new lec19();
+        int n = sc.nextInt();
+        int [] arr = new int[n];
+
+        System.out.println("Enter elements in array: ");
+        for (int i=0; i<arr.length; i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Your array is: ");
+        obj.printArray(arr);
+        System.out.println();
+
+        System.out.println("The prefix sum of given array is: ");
+        int [] ans = obj.PrefixSum(arr);
+        obj.printArray(ans);
+    }
+}
