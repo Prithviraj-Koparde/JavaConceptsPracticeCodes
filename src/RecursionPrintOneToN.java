502ddbe19a6f894778185116f4ff1f34f3b1ac56
Prithviraj-Koparde <prithvirajkoparde@gmail.com>
2025-09-23 14:53:48 +0530

    Recursion intro RecursionPrintNToOne numbers

--- src/RecursionPrintOneToN.java
+++ src/RecursionPrintOneToN.java
@@ -1,12 +1,31 @@
 import java.util.Scanner;
 class lec27{
+
+    // 1 to n
     static void printNumbers(int n){       // function being called on line 8 by updating or decreasing the value of n by n-1
+        // base case
         if (n == 1){
             System.out.println(n);
             return;
         }
+        // recursive case
         printNumbers(n-1);              // function calling itself
+        //self work
         System.out.println(n);
+    }
+
+    // n to 1
+    static void printDecrease(int n){
+        // Base case
+        if (n == 1){
+            System.out.println(n);
+            return;
+        }
+
+        // self work
+        System.out.println(n);
+        // recursive case
+        printDecrease(n-1);
     }
 }
 
@@ -16,5 +35,8 @@ public class RecursionPrintOneToN {
         lec27 obj = new lec27();
         int n = sc.nextInt();
         obj.printNumbers(n);
+
+        System.out.println(" this is n to 1");
+        obj.printDecrease(n);
     }
 }
