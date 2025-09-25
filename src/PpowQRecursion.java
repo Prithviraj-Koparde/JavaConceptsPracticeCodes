800960042f4b47ebf9eedbf588ba40bfa13a4bd8
Prithviraj-Koparde <prithvirajkoparde@gmail.com>
2025-09-25 12:38:23 +0530

    PpowQRecursion alternate method

--- src/PpowQRecursion.java
+++ src/PpowQRecursion.java
@@ -6,11 +6,20 @@ class lec29b {
         // recursive case and self work
         return Pow(p, q - 1) * p;
     }
+
+    // Alternate method
+    static int Pow1(int p, int q) {
+        if (q == 0) return 1;
+        int smallans = Pow1(p, q / 2);
+        if (q % 2 == 0) return smallans * smallans; // if even
+        // odd
+        return p * smallans * smallans;
+    }
 }
 
 public class PpowQRecursion {
     public static void main(String[] args) {
         lec29b obj = new lec29b();
-        System.out.println(obj.Pow(2, 3));
+        System.out.println(obj.Pow(2, 3) + " " + obj.Pow1(2, 4));
     }
 }
