9158d2d82ed960124c6bd73fb455bd69133c23a2
Prithviraj-Koparde <prithvirajkoparde@gmail.com>
2025-10-06 12:53:21 +0530

    Print sum of all elements of array

--- src/PrintArrEleRecursively.java
+++ src/PrintArrEleRecursively.java
@@ -1,6 +1,8 @@
 import java.lang.Math;
+
 // print array elements recursively
 // print max value in array
+// Print sum of all elements of array
 class lec32 {
     static void PrintArr(int[] arr, int index) {
         int n = arr.length;
@@ -15,11 +17,21 @@ class lec32 {
     static int PrintMax(int[] arr, int idx) {
         int n = arr.length;
         //base case
-        if (idx == n-1) return arr[idx];
+        if (idx == n - 1) return arr[idx];
         // recursive case
         int smallAns = PrintMax(arr, idx + 1);
         // self work
         return Math.max(arr[idx], smallAns);
+    }
+
+    static int Sum(int[] arr, int idx) {
+        int n = arr.length;
+        //base case
+        if (idx == n) return 0;
+        //recursive case
+        int smallAns = Sum(arr, idx + 1);
+        // self work
+        return smallAns + arr[idx];
     }
 }
 
@@ -28,6 +40,7 @@ public class PrintArrEleRecursively {
         lec32 obj = new lec32();
         int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         obj.PrintArr(arr, 0);
-        System.out.println("MAXIMUM ELEMENT IN ARRAY IS "+obj.PrintMax(arr, 0));
+        System.out.println("MAXIMUM ELEMENT IN ARRAY IS " + obj.PrintMax(arr, 0));
+        System.out.println(obj.Sum(arr, 0));
     }
 }
