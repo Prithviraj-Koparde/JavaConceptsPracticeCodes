566e6208fe24c8cef5a3895c4f832d439f32e767
Prithviraj-Koparde <prithvirajkoparde@gmail.com>
2025-10-06 12:32:15 +0530

    print max value in array

--- src/PrintArrEleRecursively.java
+++ src/PrintArrEleRecursively.java
@@ -1,20 +1,33 @@
+import java.lang.Math;
 // print array elements recursively
-class lec32{
-    static void PrintArr(int [] arr, int index){
+// print max value in array
+class lec32 {
+    static void PrintArr(int[] arr, int index) {
         int n = arr.length;
         // base case
-        if (arr.length==0 || index == arr.length) return;
+        if (n == 0 || index == n) return;
         //self work
         System.out.println(arr[index]);
         // recursive case
-        PrintArr(arr,index+1);
+        PrintArr(arr, index + 1);
+    }
+
+    static int PrintMax(int[] arr, int idx) {
+        int n = arr.length;
+        //base case
+        if (idx == n-1) return arr[idx];
+        // recursive case
+        int smallAns = PrintMax(arr, idx + 1);
+        // self work
+        return Math.max(arr[idx], smallAns);
     }
 }
 
 public class PrintArrEleRecursively {
     public static void main(String[] args) {
         lec32 obj = new lec32();
-        int [] arr = {1,2,3,4,5,6,7,8,9,10};
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         obj.PrintArr(arr, 0);
+        System.out.println("MAXIMUM ELEMENT IN ARRAY IS "+obj.PrintMax(arr, 0));
     }
 }
