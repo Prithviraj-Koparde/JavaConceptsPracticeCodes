e57d5fb9d33bbf05e7f853e8ed92bc1ed5cba489
Prithviraj-Koparde <prithvirajkoparde@gmail.com>
2025-10-08 20:10:25 +0530

    tell if string is palindrome or not method 1

--- src/ReverseStringRecursively.java
+++ src/ReverseStringRecursively.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 // reverse the given string
+// tell if string is palindrome or not
 class lec34a {
     static String Reverse(String s, int idx) {
         //Base case
@@ -18,5 +19,12 @@ public class ReverseStringRecursively {
         String s = sc.nextLine();
         lec34a obj = new lec34a();
         System.out.println(obj.Reverse(s, 0));
+        // method 1 for checking palindrome
+        String rev = obj.Reverse(s,0);
+        if (rev.equals(s)){
+            System.out.println("Palindrome");
+        }else {
+            System.out.println("Not Palindrome");
+        }
     }
 }
