6fe5948c32c046c03b71a852005c8f24cbbdb165
Prithviraj-Koparde <prithvirajkoparde@gmail.com>
2025-10-11 20:12:53 +0530

    print all subsequences of string "abc"

---
+++ src/PrintStringSubsequencesRec.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+
+// print all subsequences of string "abc" in arraylist order doesn't matter for example output ="abc" "ab" "ac" "a" "bc" "b" "c" " "
+class lec35 {
+    static ArrayList<String> SSQ(String s) {
+        ArrayList<String> ans = new ArrayList<>();
+        //base case
+        if (s.length() == 0) {
+            ans.add("");
+            return ans;
+        }
+        //recursive case
+        ArrayList<String> smallAns = SSQ(s.substring(1));
+        char currChar = s.charAt(0);
+        //self work
+        for (String ss : smallAns) {
+            ans.add(ss);
+            ans.add(currChar + ss);
+        }
+        return ans;
+    }
+}
+
+public class PrintStringSubsequencesRec {
+    public static void main(String[] args) {
+        lec35 obj = new lec35();
+        ArrayList<String> ans = obj.SSQ("abc");
+        for (String ss : ans) {
+            System.out.println(ss);
+        }
+    }
+}
