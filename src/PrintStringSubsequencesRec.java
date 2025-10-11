343027f9115bcf61f5fc932262e208fb73dae248
Prithviraj-Koparde <prithvirajkoparde@gmail.com>
2025-10-11 20:57:41 +0530

    print all subsequences of ant given string

--- src/PrintStringSubsequencesRec.java
+++ src/PrintStringSubsequencesRec.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 
 // print all subsequences of string "abc" in arraylist order doesn't matter for example output ="abc" "ab" "ac" "a" "bc" "b" "c" " "
+// print all subsequences of given string not only abc
 class lec35 {
     static ArrayList<String> SSQ(String s) {
         ArrayList<String> ans = new ArrayList<>();
@@ -19,6 +20,19 @@ class lec35 {
         }
         return ans;
     }
+
+    public void SSQ1(String s, String currAns) {
+        // base case
+        if (s.length() == 0) {
+            System.out.println(currAns);
+            return;
+        }
+        char currChar = s.charAt(0);
+        // self work and recursive case
+        String remString = s.substring(1);
+        SSQ1(remString, currAns + currChar); // include currChar
+        SSQ1(remString, currAns); // exclude currChar
+    }
 }
 
 public class PrintStringSubsequencesRec {
@@ -28,5 +42,9 @@ public class PrintStringSubsequencesRec {
         for (String ss : ans) {
             System.out.println(ss);
         }
+
+        System.out.println("+++++++++");
+        String ans1 = "abcd";
+        obj.SSQ1(ans1, "");
     }
 }
