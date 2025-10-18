a2fd2f7b8c443749a6445bec68ef19cbde9f27ca
Prithviraj-Koparde <prithvirajkoparde@gmail.com>
2025-10-18 19:59:41 +0530

    optimized bubble sort array for nearly sorted arrays

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -5,12 +5,15 @@ public class BubbleSort {
     static void bubbleSort(int[] arr) {
         int n = arr.length;
         for (int i = 0; i < n - 1; i++) {
+            boolean swapped = false;             // has swapped performed
             for (int j = 0; j < n - i - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
+                    swapped = true;             // if swap is performed swapped value will be set to true
                 }
+                if (swapped == false) return;   // if it remains false then it means no swapping is done so the arrays is sorted
             }
         }
     }
