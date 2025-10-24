2c6c6f78094562141f0cf8d8ee08ff7c88cd526a
Prithviraj-Koparde <prithvirajkoparde@gmail.com>
2025-10-24 20:15:40 +0530

    InsertionSort

--- src/InsertionSort.java
+++ src/InsertionSort.java
@@ -1,24 +1,25 @@
-class lec39{
-    static void Sort(int [] arr){
+class lec39 {
+    static void Sort(int[] arr) {
         int n = arr.length;
 
-        for (int i = 1; i<n; i++){
-            for (int j = i; j>0; j--){
-                if (arr[j] < arr[j-1]){
+        for (int i = 1; i < n; i++) {
+            for (int j = i; j > 0; j--) {
+                if (arr[j] < arr[j - 1]) {
                     int temp = arr[j];
-                    arr[j] = arr[j-1];
-                    arr[j-1] = temp;
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
                 }
             }
         }
     }
 }
+
 public class InsertionSort {
     public static void main(String[] args) {
-        int [] arr = {3,4,5,2,9,3,5,4,2,6,5,7,8,6,10};
+        int[] arr = {3, 4, 5, 2, 9, 3, 5, 4, 2, 6, 5, 7, 8, 6, 10};
         lec39 obj = new lec39();
         obj.Sort(arr);
-        for(int i : arr){
+        for (int i : arr) {
             System.out.println(i);
         }
     }
