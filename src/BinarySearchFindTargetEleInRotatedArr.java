32e35f9ac1c02be1dae3989d4c165a43b087c9d1
Prithviraj-Koparde <prithvirajkoparde@gmail.com>
2025-11-13 11:51:32 +0530

    BinarySearch Find Target Ele In Rotated Arr in which duplicate elements are present

--- src/BinarySearchFindTargetEleInRotatedArr.java
+++ src/BinarySearchFindTargetEleInRotatedArr.java
@@ -10,6 +10,41 @@ class lec46a {
             if (arr[mid] == target) return mid;
 
             // If right half is sorted
+            if (arr[mid] < arr[end]) {
+                if (target > arr[mid] && target <= arr[end]) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
+            // Left half is sorted
+            else {
+                if (target > arr[start] && target < arr[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+
+    // method for duplicate rotated array
+    static int FindTarget_(int[] arr, int target) {
+        int start = 0, end = arr.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[start] == arr[mid] && arr[mid] == arr[end]) {
+                start++;
+                end--;
+            }
+
+            // If right half is sorted
             if (arr[mid] <= arr[end]) {
                 if (target > arr[mid] && target <= arr[end]) {
                     start = mid + 1;
@@ -26,7 +61,6 @@ class lec46a {
                 }
             }
         }
-
         return -1;
     }
 }
@@ -34,7 +68,11 @@ class lec46a {
 public class BinarySearchFindTargetEleInRotatedArr {
     public static void main(String[] args) {
         int[] arr = {5, 6, 1, 2, 3, 4};
-        int ans = lec46a.FindTarget(arr, 5);
+        int[] arr1 = {1, 1, 1, 2, 2, 3, 1};
+        lec46a obj = new lec46a();
+        int ans = obj.FindTarget(arr, 5);
         System.out.println(ans);
+        int ans_ = obj.FindTarget_(arr1, 2);
+        System.out.println(ans_);
     }
 }
